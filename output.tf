057b865b0ed4c7617066545c30a4f757e3918895
GiuseppeDiPalma <dipalmagiuseppe1@gmail.com>
2024-06-04 16:53:11 +0200

    fix output

--- output.tf
+++ output.tf
@@ -1,16 +1,16 @@
 output "aws_ec2_ami_id" {
   description = "The AMI ID of the Amazon Linux 2 AMI"
-  value       = data.aws_ami.amzn2.id
+  value       = data.aws_ami.amzn2.image_owner_alias
 }
 
 output "ec2_first_app_private_ip" {
-  description = "Private Ip of the EC2 instances"
+  description = "First App Private IP"
   value       = aws_instance.first_app.private_ip
 }
 
 output "ec2_second_app_private_ip" {
-  description = "Private Ip of the EC2 instances"
-  value       = [aws_instance.second_app.private_ip]
+  description = "Second App Private IP"
+  value       = aws_instance.second_app.private_ip
 }
 
 output "elb_dns_name" {
