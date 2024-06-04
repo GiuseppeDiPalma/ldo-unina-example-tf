979282ffb7681aca2442df2c621445fcdc189375
GiuseppeDiPalma <dipalmagiuseppe1@gmail.com>
2024-06-04 17:50:27 +0200

    add stickiness code but DISABLE!

--- alb.tf
+++ alb.tf
@@ -46,6 +46,13 @@ resource "aws_lb_target_group" "sample_tg" {
   port        = 80
   protocol    = "HTTP"
   vpc_id      = aws_vpc.vpc.id
+
+  stickiness {
+    enabled         = false
+    type            = "lb_cookie"
+    cookie_duration = 120
+  }
+
   health_check {
     healthy_threshold   = 2
     unhealthy_threshold = 2
