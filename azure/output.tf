8f9f6bd1e7b664ef31d45ceaa4c42ce010c83518
GiuseppeDiPalma <dipalmagiuseppe1@gmail.com>
2024-06-17 19:55:00 +0200

    new fix for image and azure fix

--- azure/output.tf
+++ azure/output.tf
@@ -1,7 +1,7 @@
-output "az_key_vault_name" {
-  description = "The name of the Azure Key Vault"
-  value       = azurerm_key_vault.key_vault.name
-}
+# output "az_key_vault_name" {
+#   description = "The name of the Azure Key Vault"
+#   value       = azurerm_key_vault.key_vault.name
+# }
 
 output "az_public_ip" {
   description = "The public IP address of the Azure VM"
--- azure/vm.tf
+++ azure/vm.tf
@@ -1,7 +1,6 @@
-# data "azurerm_image" "ubuntu" {
-#   name                = "centos-7_9"
-#   resource_group_name = azurerm_resource_group.rg.name
-# }
+data "template_file" "cloud_init_config" {
+  template = file("${path.module}/cloud_init_config.yaml")
+}
 
 resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
   name                  = "my-first-terraform-VM"
@@ -11,18 +10,28 @@ resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
   network_interface_ids = [azurerm_network_interface.private_a_nic.id]
   size                  = "Standard_DS1_v2"
 
+
   os_disk {
     name                 = "my-first-terraform-OsDisk"
     caching              = "ReadWrite"
     storage_account_type = "Premium_LRS"
   }
 
+  # source_image_reference {
+  #   publisher = "OpenLogic"
+  #   offer     = "CentOS"
+  #   sku       = "8_5-gen2"
+  #   version   = "latest"
+  # }
+
+  # https://az-vm-image.info/
   source_image_reference {
     publisher = "OpenLogic"
     offer     = "CentOS"
     sku       = "8_5-gen2"
     version   = "latest"
   }
+
 
   admin_username                  = "secureadmin"
   disable_password_authentication = true
@@ -32,5 +41,32 @@ resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
     public_key = tls_private_key.key_pair.public_key_openssh
   }
 
-  custom_data = base64encode(var.user_data)
+  custom_data = base64encode(<<CLOUD_INIT
+#cloud-config
+write_files:
+-   content: |
+      <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+ 
+      <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+      <head>
+        <title>Nginx HTTP Server on AZURE</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+      </head>
+      <body>
+      <h1>Welcome!</h1>
+      <h2>Welcome!</h2>
+        <div>Welcome to <b>nginx</b> on Oracle Cloud Infrastructure!</div>
+      </body>
+      </html>
+    owner: opc:opc
+    path: /home/opc/index.html
+runcmd:
+-   /bin/yum install -y nginx
+-   /bin/systemctl start nginx
+-   /bin/firewall-offline-cmd --add-port=80/tcp
+-   /bin/systemctl restart firewalld
+-   cp /usr/share/nginx/html/index.html /usr/share/nginx/html/index.original.html
+-   cat /home/opc/index.html > /usr/share/nginx/html/index.html
+  CLOUD_INIT
+  )
 }
